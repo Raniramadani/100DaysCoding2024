14a6a75afe10945028764fcb86a1df162879c8c8
aulia <auliaramadaniar08@gmail.com>
2024-09-02 22:39:02 +0800

    hari ke 1

---
+++ hari001/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari001;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        //macam-macam cara menampilkan taks
+        System.out.println("Helow world");
+        System.out.println("helow");
+        System.out.println("int print");
+        
+        String nama = "aulia";
+        int umur = 18;
+        
+        System.out.printf("nama: %s, umur: %d\n", nama, umur);
+        
+        //"f" dalam printf mengartikan "format"
+}
+}
