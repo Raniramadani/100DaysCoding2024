40dfd852ed6898739941b3d654df96e95b53599f
aulia <auliaramadaniar08@gmail.com>
2024-09-03 20:05:25 +0800

    hari ke 2

---
+++ hari002/NewClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari002;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        String nama = "Aulia Ramadani";
+        byte umur = 18;
+        int angkatan = 23;
+        long tahunlahir = 2005;
+        double tb = 1.50;
+        float bb = 57.0f;
+        char kelas = 'D';
+        short tahun = 2024;
+        //  boolean fakta = true;
+
+        System.out.printf("nama: %s, umur: %s, angkatan %s, tahunlahir %s, tb %s, bb %s, kelas %s, tahun %d\n ", nama, umur, angkatan, tahunlahir, tb, bb, kelas, tahun);
+    }
+}
