93f77dbd74a88b7a0ea5d58cd0fbeefc9e2bd18b
aulia <auliaramadaniar08@gmail.com>
2024-09-04 19:32:11 +0800

    hari ke 003

---
+++ hari003/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari003;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        String nama = "aulia ramadani";
+        byte umur = 18;
+        char jender = 'p';
+        
+        System.out.println("nama = " + nama);
+        System.out.println("umur = " + umur);
+        System.out.println("jender = " + jender);
+    }
+    
+}
