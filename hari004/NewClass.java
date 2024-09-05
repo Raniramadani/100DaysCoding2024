d7b8b801d6b5ad1e2b0462279124ec4259826c08
aulia <auliaramadaniar08@gmail.com>
2024-09-05 21:24:16 +0800

    hari ke 003

---
+++ hari004/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari004;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("aulia");
+        System.out.println("18");
+        System.out.println("D informatika");
+        System.out.printf("D0223033 %n");
+        System.out.printf("2024 %n");
+        System.out.printf("8/10/1005 %n");
+        
+    }
+}
