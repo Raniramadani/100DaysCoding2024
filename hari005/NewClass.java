85563578118f13eb0e58d03464efa5444ebd0c09
aulia <auliaramadaniar08@gmail.com>
2024-09-06 21:14:12 +0800

    hari005

--- hari005/NewClass.java
+++ hari005/NewClass.java
@@ -9,5 +9,17 @@ package hari005;
  * @author ACER
  */
 public class NewClass {
+     public static void main(String[] args) {
+         //"%n" atau \n digunakan sebagai pemisah baris dalam printf()
+    //"f" dalam printf mengartikan "format"
+    //"print line", yang berarti setelah argumen dicetak, maka akan berlanjut ke baris berikutnya
+    System.out.println("aulia ramadani");
+    //"print" akan menampilkan tulisan pada layar dalam satu baris yang sama
+    //"%s" memberi tahu printf untuk mencetak string
+    //"psvm" method ini adalah untuk menjalankan program(puntu dari sebuah perogram)
+        
+    }
+    
+    
     
 }
