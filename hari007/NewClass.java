66cddf5073d3245f476dbf33dcf95db6d5cd94fc
aulia <auliaramadaniar08@gmail.com>
2024-09-08 00:54:14 +0800

    hari007

---
+++ hari007/NewClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari007;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        byte angka, umur;
+        float bb;
+        char golDarah;
+        angka = 10;
+        umur = 18;
+        bb = 57.0f;
+        golDarah = 'o';
+        System.out.println("angka = " + angka);
+        System.out.println("umur = " + umur + "thn");
+        System.out.println("bb = " + bb + "kg");
+        System.out.println("golDarah = " + golDarah);
+    }
+}
