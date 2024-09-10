a1465cc0ecdfd07b7e000166e2810ec78f1ecc12
aulia <auliaramadaniar08@gmail.com>
2024-09-10 20:51:21 +0800

    hari009

---
+++ hari009/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari009;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        //"final" nlai tdk bisa di ubah sebelum keyword final
+        final int umur = 18;
+        System.out.println(umur);
+
+    }
+}
