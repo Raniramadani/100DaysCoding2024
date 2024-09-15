3d011e8dc7d7c0f286255c1093cb4622e55d4fec
aulia <auliaramadaniar08@gmail.com>
2024-09-15 19:55:48 +0800

    hari014

--- hari014/NewClass.java
+++ hari014/NewClass.java
@@ -3,7 +3,7 @@ package hari014;
 public class NewClass {
 
     public static void main(String[] args) {
-        long noHp = 6282197322379L;
-        System.out.println("noHp saya : " + noHp);
+        long hasilTransfer = 1500000000L;
+        System.out.println("hasil Transfer : " + hasilTransfer);
     }
 }
