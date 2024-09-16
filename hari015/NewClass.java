001c38684326b795731b22f5324cfa673e176656
aulia <auliaramadaniar08@gmail.com>
2024-09-16 18:48:21 +0800

    hari015

---
+++ hari015/NewClass.java
@@ -0,0 +1,9 @@
+package hari015;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        double tinggiBadan = 15.0;
+        System.out.println("tinngi abadan saya: " + tinggiBadan);
+    }
+}
