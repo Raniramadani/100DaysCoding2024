0f5a9213d2ae7e1ad02ffaf27c2e290fd8f636f2
aulia <auliaramadaniar08@gmail.com>
2024-09-17 14:09:00 +0800

    hari016

--- hari015/NewClass.java
+++ hari015/NewClass.java
@@ -3,7 +3,7 @@ package hari015;
 public class NewClass {
 
     public static void main(String[] args) {
-        double tinggiBadan = 150.0;
+        double tinggiBadan = 150;
         System.out.println("tinngi badan saya: " + tinggiBadan);
     }
 }
---
+++ hari016/NewClass.java
@@ -0,0 +1,9 @@
+package hari016;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        float beratBadan = 57f;
+        System.out.println("berat badan: " + beratBadan);
+    }
+}
