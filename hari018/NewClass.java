08a30bbd3ca9515660574306c570dd1f188a1488
aulia <auliaramadaniar08@gmail.com>
2024-09-19 20:23:32 +0800

    hari018

---
+++ hari018/NewClass.java
@@ -0,0 +1,9 @@
+package hari018;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        boolean statusMahasiswa = true;
+        System.out.println("apakah saya masih mahasiswa: " + statusMahasiswa);
+    }
+}
