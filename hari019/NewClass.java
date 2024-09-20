2075f5db7a116a7f742fc18e97ea985150b03476
aulia <auliaramadaniar08@gmail.com>
2024-09-20 23:13:37 +0800

    hari019

---
+++ hari019/NewClass.java
@@ -0,0 +1,10 @@
+package hari019;
+public class NewClass {
+    public static void main(String[] args) {
+    String nama, alumni;
+    nama = "aulia ramadani";
+    alumni ="smkn 2 majene";
+    System.out.println("nama: " + nama);
+        System.out.println("alumni: " + alumni);
+    }
+}
