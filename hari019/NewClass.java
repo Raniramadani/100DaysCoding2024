4cfd62eac702e2936bdc563840e1a1818d8d43bd
aulia <auliaramadaniar08@gmail.com>
2024-09-21 22:01:33 +0800

    hari019

--- hari019/NewClass.java
+++ hari019/NewClass.java
@@ -1,10 +1,12 @@
 package hari019;
+
 public class NewClass {
+
     public static void main(String[] args) {
-    String nama, alumni;
-    nama = "aulia ramadani";
-    alumni ="smkn 2 majene";
-    System.out.println("nama: " + nama);
-        System.out.println("alumni: " + alumni);
+        String nama, alumni;
+        nama = "aulia ramadani";
+        alumni = "SMKN 2 MAJENE";
+        System.out.println(nama.substring(6));
+        System.out.println(alumni.toLowerCase());
     }
 }
---
+++ hari020/NewClass.java
@@ -0,0 +1,12 @@
+package hari020;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        Byte angka1, angka2;
+        angka1 = 18;
+        angka2 = 18;
+        System.out.println(angka1.compareTo(angka2));
+        System.out.println(angka2.byteValue());
+    }
+}
