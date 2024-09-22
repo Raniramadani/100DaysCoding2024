850b7102fe98d4a854a4beb69303640cb18f7a1d
aulia <auliaramadaniar08@gmail.com>
2024-09-22 23:16:00 +0800

    hari021

---
+++ hari021/NewClass.java
@@ -0,0 +1,13 @@
+package hari021;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        Short tahun, tahun1;
+        tahun = 2024;
+        tahun1 = 2005;
+        System.out.println(tahun.shortValue());
+        String str = Short.toString(tahun1);
+        System.out.println(str);
+    }
+}
