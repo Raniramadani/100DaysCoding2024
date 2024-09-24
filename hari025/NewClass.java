d7f1f17e9d93c635f863f658dbc694c51e2b775d
aulia <auliaramadaniar08@gmail.com>
2024-09-25 06:46:39 +0800

    hari025

---
+++ hari025/NewClass.java
@@ -0,0 +1,12 @@
+
+package hari025;
+
+public class NewClass {
+    public static void main(String[] args) {
+        Double a, b;
+        a = 18.3;
+        b = 19.0;
+        System.out.println(a.compareTo(b));
+        System.out.println(a.toString());
+    }
+}
