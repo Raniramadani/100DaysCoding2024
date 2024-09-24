586c080d1a6285d8e0327cf32b5f41bc4f29273b
aulia <auliaramadaniar08@gmail.com>
2024-09-24 18:36:57 +0800

    hari022

---
+++ hari023/NewClass.java
@@ -0,0 +1,15 @@
+package hari023;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        Long a, b;
+        a = 2005L;
+        b = 2002L;
+        long sum = Long.sum(a, b);
+        System.out.println(sum);
+        long maxValue = Long.max(a, b);
+        System.out.println(maxValue);
+    }
+
+}
