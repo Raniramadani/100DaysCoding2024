f8af3f9c26b5bc4764a06969d908cd85789164ef
aulia <auliaramadaniar08@gmail.com>
2024-09-25 06:14:08 +0800

    hari024

---
+++ hari024/NewClass.java
@@ -0,0 +1,10 @@
+package hari024;
+public class NewClass {
+    public static void main(String[] args) {
+        Float a, b;
+        a = 15.0f;
+        b = 13.3f;
+        System.out.println(a.compareTo(a));
+        System.out.println(a.isNaN());
+    }
+}
