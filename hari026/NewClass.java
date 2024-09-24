24cc71d2d255a199e85b9d59c83ccd4a56204f75
aulia <auliaramadaniar08@gmail.com>
2024-09-25 07:01:44 +0800

    hari026

---
+++ hari026/NewClass.java
@@ -0,0 +1,12 @@
+
+package hari026;
+
+public class NewClass {
+    public static void main(String[] args) {
+        Character a, b;
+        a = 'd';
+        b = 'p';
+        System.out.println(a.equals(b));
+        System.out.println(b.charValue());
+    }
+}
