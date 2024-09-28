3feba5baeaf6852a36584e1150d3b5a402ef051b
aulia <auliaramadaniar08@gmail.com>
2024-09-28 22:16:27 +0800

    hari027

---
+++ hari027/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari027;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("selamat datang di peternakan pak widi!");
+        final int jumlahHewan = 30; // "final" tidak dapat di ubah
+        String namaHewan = "harimau";
+        namaHewan = "singa";
+        float hargaHewan = 12.5f;
+        System.out.printf("bapak widi memiliki hewan %s berjumlah %d dan harga perekor %f ",namaHewan,jumlahHewan,hargaHewan);
+        
+    }
+}
