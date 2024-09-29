2b1dc3e8efc92febd30099a3906ed984942d15df
aulia <auliaramadaniar08@gmail.com>
2024-09-29 22:56:05 +0800

    hari028

---
+++ hari028/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari028;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        Boolean a, b;
+        a = true;
+        b = false;
+        System.out.println(a.equals(b));
+        System.out.println(b.compareTo(a));
+    }
+}
