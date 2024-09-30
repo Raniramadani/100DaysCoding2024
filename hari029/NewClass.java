f98c1f3a1357e60a608557c7163a0fc673294e3f
aulia <auliaramadaniar08@gmail.com>
2024-09-30 23:43:38 +0800

    hari029

--- hari029/NewClass.java
+++ hari029/NewClass.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package hari029;
 
-/**
- *
- * @author ACER
- */
+package hari029;
+import java.math.BigInteger;
 public class NewClass {
-    
+    public static void main(String[] args) {
+        BigInteger a = BigInteger.valueOf(155);
+        BigInteger b = BigInteger.valueOf(200);
+        System.out.println(b.subtract(a));
+        System.out.println(a.add(b));
+    }
 }
