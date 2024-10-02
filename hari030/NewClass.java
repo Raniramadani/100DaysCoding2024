fcba61b934514e0847e492348e16b14116546dee
aulia <auliaramadaniar08@gmail.com>
2024-10-02 22:53:34 +0800

    hari031

--- hari030/NewClass.java
+++ hari030/NewClass.java
@@ -1,11 +1,14 @@
 package hari030;
+
 import java.math.BigDecimal;
+
 public class NewClass {
+
     public static void main(String[] args) {
         BigDecimal a = new BigDecimal("15.0");
         BigDecimal b = new BigDecimal("15.0");
         System.out.println(a.add(b));
         System.out.println(b.equals(a));
     }
-    
+
 }
---
+++ hari031/NewClass.java
@@ -0,0 +1,11 @@
+package hari031;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        String a = "rany";
+        int b = Integer.parseInt(a);
+        System.out.println(b);
+    }
+
+}
