84bcdbbbba3042ea67cd927a7b34cb2e731f5790
aulia <auliaramadaniar08@gmail.com>
2024-10-03 18:12:13 +0800

    hari032

--- hari031/NewClass.java
+++ hari031/NewClass.java
@@ -3,7 +3,7 @@ package hari031;
 public class NewClass {
 
     public static void main(String[] args) {
-        String a = "rany";
+        String a = "18";
         int b = Integer.parseInt(a);
         System.out.println(b);
     }
---
+++ hari032/NewClass.java
@@ -0,0 +1,30 @@
+package hari032;
+public class NewClass {
+    public static void main(String[]args){
+        byte a = 12;
+        short b = 13;
+        long c = 18l;
+        int d = 24;
+        float e = 13.3f;
+        char f = 'a';
+        double g = 27.8;
+        boolean h = true;
+        String i = String.valueOf(a);
+        String j = String.valueOf(b);
+        String k = String.valueOf(c);
+        String l = String.valueOf(d);
+        String m = String.valueOf(e);
+        String n = String.valueOf(f);
+        String o = String.valueOf(g);
+        String p = String.valueOf(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
+        
+    }
+}
