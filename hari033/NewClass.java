b1fb944c0637f9f73bc5dc6b3389ad9bc4adcec5
aulia <auliaramadaniar08@gmail.com>
2024-10-04 23:51:40 +0800

    hari033

--- hari033/NewClass.java
+++ hari033/NewClass.java
@@ -9,5 +9,15 @@ package hari033;
  * @author ACER
  */
 public class NewClass {
-    
+
+    public static void main(String[] args) {
+        int a = 100;
+        long b = a;
+        float c = a;
+        double d = a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
 }
