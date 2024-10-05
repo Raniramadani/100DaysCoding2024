e8fcde2ecdd1f71ea824695340c676ab4566005a
aulia <auliaramadaniar08@gmail.com>
2024-10-05 23:26:27 +0800

    hari034

---
+++ hari034/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari034;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("maukan nama dan umur");
+        String nama = sc.nextLine();
+        int umur = sc.nextInt();
+
+        System.out.println(nama);
+        System.out.println(umur);
+    }
+}
