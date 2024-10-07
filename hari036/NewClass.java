60729c3744d62a69a7b01c1825b1c7f26d5d402f
aulia <auliaramadaniar08@gmail.com>
2024-10-07 21:56:20 +0800

    hari036

---
+++ hari036/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari036;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int a = 33;
+        int b = 33;
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(b%a);
+        
+    }
+}
