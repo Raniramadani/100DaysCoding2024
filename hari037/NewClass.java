7741367991cc2298937f75932ac75815a12976f3
aulia <auliaramadaniar08@gmail.com>
2024-10-08 21:19:37 +0800

    hari037

---
+++ hari037/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari037;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masuka angka: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("pengurangan");
+        System.out.println(a-b);
+        System.out.println("penjumlahan");
+        System.out.println(a+b);
+    }
+   
+}
