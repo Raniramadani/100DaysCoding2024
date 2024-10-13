1b4556d08b109f4361d38ff65b0da5926cfb03a7
aulia <auliaramadaniar08@gmail.com>
2024-10-13 21:02:54 +0800

    hari041

---
+++ hari042/NewClass.java
@@ -0,0 +1,11 @@
+package hari042;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 25;
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
