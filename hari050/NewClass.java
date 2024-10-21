6ca472d993009f7129a849919753d8530f404b93
aulia <auliaramadaniar08@gmail.com>
2024-10-21 20:58:17 +0800

    hari50

---
+++ hari050/NewClass.java
@@ -0,0 +1,16 @@
+package hari050;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        char a = 'p';
+        if (a == 'p') {
+            System.out.println("maka ini perempuan");
+        } else if (a == 'L') {
+            System.out.println("maka ini laki laki");
+        } else {
+            System.out.println("normal");
+
+        }
+    }
+}
