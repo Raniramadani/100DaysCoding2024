4e45f0fd38ef6deac5386cc25f07b7b8c5a9cd5b
aulia <auliaramadaniar08@gmail.com>
2024-10-22 19:29:48 +0800

    hari051

---
+++ hari051/NewClass.java
@@ -0,0 +1,15 @@
+package hari051;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        int hari = 2;
+        switch (hari) {
+            case 1:
+                System.out.println("rabu");
+            case 2:
+                System.out.println("minggu");
+        }
+
+    }
+}
