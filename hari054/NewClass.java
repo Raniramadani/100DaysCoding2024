17dfd7f731b267a3cb8e833fff757ffaa2636afe
aulia <auliaramadaniar08@gmail.com>
2024-10-25 23:11:18 +0800

    hari054

---
+++ hari054/NewClass.java
@@ -0,0 +1,8 @@
+package hari054;
+public class NewClass {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
