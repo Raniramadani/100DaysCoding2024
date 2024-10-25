b911852ec0a82acee37febe8483915a4cd68d0bc
aulia <auliaramadaniar08@gmail.com>
2024-10-25 23:18:55 +0800

    hari054

--- hari054/NewClass.java
+++ hari054/NewClass.java
@@ -1,8 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package hari054;
+
+/**
+ *
+ * @author ACER
+ */
 public class NewClass {
     public static void main(String[] args) {
-        for (int i = 10; i >= 1 ; i--) {
+        for (int i =1; i < 20; i+=2) {
             System.out.println(i);
+            
         }
     }
 }
