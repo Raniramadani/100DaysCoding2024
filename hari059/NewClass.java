92df519f8d433f0c8b9fc7ea02650c2742e6b449
aulia <auliaramadaniar08@gmail.com>
2024-10-30 23:44:20 +0800

    hari059

---
+++ hari059/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari059;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        int[] angka = {29,20,19,16};
+
+        for (int ang : angka) {
+            System.out.println(ang);
+        }
+    }
+}
