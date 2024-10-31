967e5519dbe26011813ae3e1c9a537acb0b84c94
aulia <auliaramadaniar08@gmail.com>
2024-10-31 22:59:36 +0800

    hari060

---
+++ hari060/NewClass.java
@@ -0,0 +1,13 @@
+package hari060;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        int ang = 0;
+        do {
+            System.out.println(ang);
+            ang++;
+        } while (ang != 10);
+
+    }
+}
