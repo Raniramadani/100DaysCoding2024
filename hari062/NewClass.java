53ab90be227a386b44cc8271306ec652f2f7117e
aulia <auliaramadaniar08@gmail.com>
2024-11-02 23:02:46 +0800

    hari062

---
+++ hari062/NewClass.java
@@ -0,0 +1,13 @@
+package hari062;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break; // Menghentikan loop ketika i sama dengan 5
+            }
+            System.out.println(i);
+        }
+    }
+}
