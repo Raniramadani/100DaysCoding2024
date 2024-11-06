00f0d239447e824afec3fc43101a48cf4f917b85
aulia <auliaramadaniar08@gmail.com>
2024-11-06 23:19:08 +0800

    hari066

--- hari066/NewClass.java
+++ hari066/NewClass.java
@@ -3,14 +3,21 @@ package hari066;
 public class NewClass {
 
     public static void main(String[] args) {
-        a("rany");
-        a("raisya");
-        a("febi");
-        a("yayat");
-        
-    }
-
-    static void a(String nama) {
-        System.out.println("haii.. "+nama);
+        int sisi = 5;
+        for (int i = 1; i <= sisi; i++) {
+            for (int j = 1; j <= sisi; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+        int panjang = 10;
+        int lebar = 5;
+        for (int i = 1; i <= lebar; i++) {
+            for (int j = 1; j <= panjang; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
