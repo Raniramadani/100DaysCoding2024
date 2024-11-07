760452e9466317cbece3e292bd30f177a18ea4e5
aulia <auliaramadaniar08@gmail.com>
2024-11-07 22:51:16 +0800

    hari067

---
+++ hari067/NewClass.java
@@ -0,0 +1,14 @@
+package hari067;
+
+public class NewClass {
+
+        public static void nama(String a) {
+            System.out.println("Hayy... " + a);
+        }
+
+        public static void main(String[] args) {
+            nama("rany");
+        }
+    }
+
+
