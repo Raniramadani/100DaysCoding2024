5b9daffb764f972bca0cb49219f173df8179ab91
aulia <auliaramadaniar08@gmail.com>
2024-11-08 23:03:31 +0800

    hari068

---
+++ hari068/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari068;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static String a (String nama){
+        return "hayy.."+nama;
+    }
+    public static void main(String[] args) {
+        String nama = a("rany");
+        System.out.println(nama);
+    }
+}
