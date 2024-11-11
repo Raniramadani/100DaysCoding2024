6ab238a74ef3ba77aa6a7837180fb6e64bb10d8c
aulia <auliaramadaniar08@gmail.com>
2024-11-11 22:32:22 +0800

    hari071

---
+++ hari071/NewClass.java
@@ -0,0 +1,21 @@
+package hari071;
+
+public class NewClass {
+    static int angka = 10;
+    public static void a() {
+        System.out.println("Ini adalah metode statis!");
+    }
+    public static int jumlah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        a();
+
+        int hasil = jumlah(5, 7);
+        System.out.println(hasil);
+        System.out.println(angka);
+    }
+}
+
+
