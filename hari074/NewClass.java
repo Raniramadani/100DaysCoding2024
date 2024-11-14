b81c2e607802f2de384c141cd0f65f4a1bc90ce3
aulia <auliaramadaniar08@gmail.com>
2024-11-14 16:46:44 +0800

    hari074

---
+++ hari074/NewClass.java
@@ -0,0 +1,8 @@
+package hari074;
+public class NewClass {
+     public static void main(String[] args) {
+        String a = "rany";
+        int b = a.length();
+        System.out.println(b);
+}
+}
