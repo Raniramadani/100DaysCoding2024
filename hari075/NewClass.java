9ae0543bb60a9a9faf9af8fcef51f1dd27011cc0
aulia <auliaramadaniar08@gmail.com>
2024-11-15 22:23:54 +0800

    hari075

---
+++ hari075/NewClass.java
@@ -0,0 +1,10 @@
+package hari075;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        String a = "AULIA RAMADANI";
+        String b = a.toLowerCase();
+        System.out.println("Lowercase String: " + b);
+    }
+}
