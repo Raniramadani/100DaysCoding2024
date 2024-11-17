9acd51bd52193d2877ffb9e40e75d1eb7e23e0bc
aulia <auliaramadaniar08@gmail.com>
2024-11-17 20:32:57 +0800

    hari077

---
+++ newpackage/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        String a = "rany";
+        String b = "aulia";
+        System.out.println(a.equals(b));
+    }
+}
