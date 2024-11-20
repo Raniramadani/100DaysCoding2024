5e3315cd98942541cedb778f1c74653e0ee0cf1f
aulia <auliaramadaniar08@gmail.com>
2024-11-20 22:59:25 +0800

    hari080

---
+++ hari080/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari080;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+        String a = " aulia ramadani ";
+        String b = a.trim();
+        System.out.println("'"+a + "'");
+        System.out.println("'" + b + "'");
+    }
+}
