a96d51d3e57ed556481b9bdca369583355509d34
aulia <auliaramadaniar08@gmail.com>
2024-11-22 20:09:08 +0800

    hari082

---
+++ hari082/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hari082;
+
+/**
+ *
+ * @author ACER
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        double a = 5.76;
+        double b = Math.ceil(a);
+        double c = Math.floor(a);
+        long d = Math.round(a);
+        System.out.printf("a = %.2f, b = %.2f, c = %.2f, d = %d\n", a, b, c, d);
+    }
+   
+}
