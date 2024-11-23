b01858aca345a4340a0250e0272368847da63606
aulia <auliaramadaniar08@gmail.com>
2024-11-23 21:47:03 +0800

    hari083

---
+++ hari083/NewClass.java
@@ -0,0 +1,10 @@
+
+package hari083;
+
+public class NewClass {
+    public static void main(String[] args) {
+        String[] nama = {"rany","feby","raisya","yayat"};
+        System.out.println(nama[2]);
+        
+    }
+}
