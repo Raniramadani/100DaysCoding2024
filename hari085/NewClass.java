6de9b1104b5886aad3780bd615cd532d86c4e39b
aulia <auliaramadaniar08@gmail.com>
2024-11-25 23:27:11 +0800

    hari085

---
+++ hari085/NewClass.java
@@ -0,0 +1,12 @@
+package hari085;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        int[] ang = {1, 2, 3, 4, 5};
+
+        for (int i = 1; i < ang.length; i++) {
+            System.out.println(ang[i]);
+        }
+    }
+}
