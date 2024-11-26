0fc9dd31db841147c3dd2dd0ea1fc3488e530578
aulia <auliaramadaniar08@gmail.com>
2024-11-26 23:37:10 +0800

    hari086

---
+++ hari086/NewClass.java
@@ -0,0 +1,11 @@
+package hari086;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        String[] nama = {"rany", "raisya"};
+        for (int i = 0; i <nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+    }
+}
