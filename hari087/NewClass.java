af55322f3829f91ca54945432fa3c0d7008f9258
aulia <auliaramadaniar08@gmail.com>
2024-11-27 23:23:17 +0800

    hari087

--- hari087/NewClass.java
+++ hari087/NewClass.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hari087;
 
-/**
- *
- * @author ACER
- */
 public class NewClass {
-    
+
+    public static void main(String[] args) {
+        String[] nama = {"rany", "raisya"};
+
+        nama[0] = "aulia";
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+    }
 }
