48cc6caef6d3d91098d380f948f7f6ea79450ecd
aulia <auliaramadaniar08@gmail.com>
2024-12-01 23:48:45 +0800

    hari091

--- hari091/NewClass.java
+++ hari091/NewClass.java
@@ -6,14 +6,14 @@ public class NewClass {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Masukkan nama Anda: ");
+        System.out.print("masukkan inputan : ");
         String nama = scanner.nextLine();
-        if (nama.equalsIgnoreCase("rany")) {
-            System.out.println("Hii.. rany!");
-        } else if (nama.equalsIgnoreCase("raisya")) {
-            System.out.println("Hi.. raisay");
+        if (nama.equalsIgnoreCase("you")) {
+            System.out.println("kamu dan aku");
+        } else if (nama.equalsIgnoreCase("I")) {
+            System.out.println("aku dan kamu");
         } else {
-            System.out.println("Nama tidak dikenali");
+            System.out.println("tidak ada");
         }
 
     }
