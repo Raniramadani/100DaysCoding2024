90628ccd05d8f67cb61807731a17109d74cc0850
aulia <auliaramadaniar08@gmail.com>
2024-12-04 22:53:04 +0800

    hari094

--- hari094/NewClass.java
+++ hari094/NewClass.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hari094;
 
-/**
- *
- * @author ACER
- */
+import java.util.Scanner;
+
 public class NewClass {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jml segitiga: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
 }
