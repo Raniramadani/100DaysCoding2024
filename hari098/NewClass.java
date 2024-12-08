d6d2cf376d29f36cb825bb4cb0c7044d90b6f956
aulia <auliaramadaniar08@gmail.com>
2024-12-08 23:35:11 +0800

    hari098

---
+++ hari098/NewClass.java
@@ -0,0 +1,28 @@
+package hari098;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        int n = 100;
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
